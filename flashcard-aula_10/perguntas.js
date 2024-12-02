0e55d3c4dcffbda43d8f9205c8482bd64ae4f38b
joao3a2604 <00001244788582SP@al.educacao.sp.gov.br>
2024-12-02 08:55:41 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é windows?',
+    'Ele possibilita a realização das mais diversas tarefas, utilizando uma interface gráfica simples e amigável. Além disso, o Windows é compatível com computadores de diversos fabricantes.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a diferença entre longitude e latitude?',
+    'Latitude é a distância em graus de um ponto na superfície terrestre até a Linha do Equador. Ela vai de 0 a 90º tanto no Hemisfério Norte quanto no Hemisfério Sul. Longitude é a distância em graus de um ponto na superfície terrestre até o Meridiano de Greenwich.'
+)
+
+criaCartao(
+    'Matematica',
+    'Para que e utilizado a equaçao do 2 grau?',
+    'possui uma importância significativa na resolução de problemas em diversas áreas, como física, engenharia, economia e ciências naturais. '
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'o que é o predicado de uma frase?',
+    'O predicado é todo termo da frase que faz referência ao sujeito.'
+)
